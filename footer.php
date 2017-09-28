0a87f4512a7e3ed1e7fdc11685e42abb3ddbf5d3
elbrag <32334193+elbrag@users.noreply.github.com>
2017-09-28 11:33:04 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+            </div> 
+            <footer>
+                  <p>© Julia Palm, Albin Johansson, Ellen Brage
+                  <br/>Jönköping University 2017
+                  </p>
+            </footer>
+
+        </main>
+    </body>
+
+</html>
