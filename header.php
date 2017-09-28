49e39edb0d434a70249dc9de406cb0cb54463e7e
elbrag <32334193+elbrag@users.noreply.github.com>
2017-09-28 11:30:50 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+	  <html lang="en">
+	  <head>
+		    <meta charset="UTF-8">
+	      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	      <link rel="stylesheet" href="css/main.css">
+	      <link href="https://fonts.googleapis.com/css?family=Lato|Open+Sans:300,400,600|Roboto:300,400,600,700 rel="stylesheet">
+	  </head>
+
+	  <body>
+		    <main>
+	      <header>
+
+	      </header>
+	          <nav id="mainmenu">
+		            <ul>
+			              <li>
+                        <a class="<?php echo ($current_page == 'index.php' || $current_page == '') ? 'active' : NULL ?>" href="index.php">Home</a>
+                    </li>
+		            </ul>
+	          </nav>
+
+	          
+            <div id="content">
+
