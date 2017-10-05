e1f1895ce576a64b275f18e75bec5cfc023f9183
elbrag <ellenbrage@outlook.com>
2017-10-05 19:06:37 +0200

    d

--- register.php
+++ register.php
@@ -20,7 +20,7 @@
     <div id="registerbox">
       <div id="aboutbutton" onclick="showAbout()">About</div>
       <div id="about">
-
+          <p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
       </div>
 
         <form action="">
