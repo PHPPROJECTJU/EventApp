c9edbba5180ea272b6a6cbefe75ba31c12e3a78a
elbrag <ellenbrage@outlook.com>
2017-10-05 09:30:01 +0200

    m

--- register.php
+++ register.php
@@ -5,6 +5,22 @@
 ?>
 
 
+<div id="registerbox">
+
+    <form>
+      <table id="registerform">
+        <tr>
+          <td><input type="text" name="username" class="searchbar" placeholder="username"></td>
+        </tr>
+        <tr>
+          <td><input type="password" name="password" class="searchbar" placeholder="password"></td>
+        </tr>
+          <td><input type="submit" class="searchbutton" name="submit" value="GO"></td>
+        </tr>
+      </table>
+    </form>
+
+</div>
 
 
 <?php
