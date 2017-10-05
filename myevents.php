87efc119f30e87f696ad10a173aa2a788226a0cc
juliapalm <32334112+juliapalm@users.noreply.github.com>
2017-10-05 10:01:55 +0200

    Revert "Merge branch 'master' of https://github.com/PHPPROJECTJU/EventApp"
    This reverts commit 5bae70dd91fa92e8b9ed8856a90c6d90d768606a, reversing
    changes made to 76b73c6266915ae5e49d1277362db933c3c59d8f.

--- myevents.php
+++ myevents.php
@@ -5,18 +5,6 @@
 ?>
 
 
-<div class="eventmain">
-    <div class="eventchoose">
-      <button class="leftevent">
-        My events
-      </button>
-      <button class="rightevent">
-        Saved events
-      </button>
-    </div>
-</div>
-
-
 
 <?php
   include("footer.php");
