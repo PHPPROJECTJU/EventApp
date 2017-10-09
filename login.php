e05b535f7b8fbab6adabab532b4be01d1a03a771
elbrag <ellenbrage@outlook.com>
2017-10-09 21:03:43 +0200

    m

--- login.php
+++ login.php
@@ -11,6 +11,47 @@
 	      <link href="https://fonts.googleapis.com/css?family=Lato|Noto+Sans:300,400,600|Roboto:300,400,600,700" rel="stylesheet">
 	  </head>
 
+<!--PHP TERRITORY---------------->
+
+<!--username: testuser. password: testing-->
+
+<?php if(isset($_POST) && !empty($_POST)) : ?>
+
+<?php
+	$getusername =  stripslashes($_POST['getusername']);
+	$getpassword =  stripslashes($_POST['getpassword']);
+
+
+	@ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
+
+	if ($db->connect_error) {
+		echo "could not connect: " . $db->connect_error;
+		exit();
+	}
+
+	$stmt = $db->prepare("SELECT UserName, Password FROM User WHERE UserName = ?");
+	$stmt->bind_param('s', $getusername);
+	$stmt->execute();
+
+    $stmt->bind_result($username, $password);
+
+
+    while ($stmt->fetch()) {
+        if (sha1($getpassword) == $password)
+		{
+			$_SESSION['username'] = $getusername;
+			header("location:index.php");
+			exit();
+		}
+    }
+
+?>
+
+<?php endif;?>
+
+<!--PHP TERRITORY end---------------->
+
+
 	  <body>
 		    <main class="red" id="main">
 
@@ -26,15 +67,15 @@
           </div>
       </div>
 
-        <form action="">
+        <form action="" method="POST">
           <table id="registerform">
             <tr>
-              <td><input type="text" name="username" class="registerbar" placeholder="username" required></td>
+              <td><input type="text" name="getusername" class="registerbar" placeholder="username" required></td>
             </tr>
             <tr>
-              <td><input type="password" name="password" class="registerbar" placeholder="password" required></td>
+              <td><input type="password" name="getpassword" class="registerbar" placeholder="password" required></td>
             </tr>
-              <td><input type="submit" class="registerbutton" name="submit" value="login"></td>
+              <td><input type="submit" class="registerbutton" name="submit" value="Sign in"></td>
             </tr>
           </table>
         </form>
