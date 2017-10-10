87351bef54561a2b2894bef5266c03f5803351ba
elbrag <ellenbrage@outlook.com>
2017-10-10 12:17:10 +0200

    tab fix

--- savedevents.php
+++ savedevents.php
@@ -1,7 +1,23 @@
 <?php
   include("config.php");
   include("header.php");
-  include("myprofileheader.php");
+
+?>
+
+<!--myprofileheader could be included, but then we wouldn't be able to have it active at the same time as the lower tabs-->
+
+
+<header id="myprofileheader">
+
+    <nav id="profileorevents">
+        <ul>
+            <li><a href="profile.php">View profile</a></li>
+            <li><a id="active" href="myevents.php">My events</a></li>
+        </ul>
+    </nav>
+</header>
+
+<?php
   include("myeventsheader.php");
 
 ?>
