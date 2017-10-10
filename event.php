8a41af4074e643c96719cbd578dd16dcf49387b1
elbrag <ellenbrage@outlook.com>
2017-10-10 09:46:11 +0200

    event page stuff

--- event.php
+++ event.php
@@ -3,9 +3,18 @@
   include("config.php");
 ?>
 
-<p class="goback">&larr;</p>
+<!--<a href="index.php#" class="goback">&larr;</a>-->
 
 <?php
+
+$EventID = trim($_GET['EventID']);
+echo '<INPUT type="hidden" name="bookid" value=' . $EventID . '>';
+
+$EventID = trim($_GET['EventID']);      // From the hidden field
+$EventID = addslashes($EventID);
+
+echo $EventID;
+
     @ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
 
     if ($db->connect_error) {
@@ -14,16 +23,24 @@
         exit();
     }
 
-    $query = "SELECT User.UserName, User.ProfilePicture, Event.EventID, Event.Title, Event.StartDate, Event.StartTime, Event.Information, Location.StreetAdress
+    $query = "SELECT User.UserName, User.ProfilePicture, Event.Title, Event.StartDate, Event.StartTime, Event.Information, Location.StreetAdress
               FROM User
               JOIN Event
               ON User.UserID=Event.UserID
               JOIN Location
-              ON Event.LocationID=Location.LocationID";
+              ON Event.LocationID=Location.LocationID
+              WHERE Event.EventID=$EventID
+              ";
+
 
     $stmt = $db->prepare($query);
-    $stmt->bind_result($UserName, $ProfilePicture, $EventID, $Title, $StartDate, $StartTime, $Information, $StreetAdress);
+    $stmt->bind_result($UserName, $ProfilePicture, $Title, $StartDate, $StartTime, $Information, $StreetAdress);
     $stmt->execute();
+
+#here I'm sending us back to index page, the idea is to land on the heading of the same event
+
+
+echo "<a href='index.php#" . urlencode($Title) . "' class='goback'>&larr;</a>";
 
     while ($stmt->fetch()) {
         echo "<div class='eventpagebox'>";
