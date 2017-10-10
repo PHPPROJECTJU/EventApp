82c5679286828980181945665d059a70cce996e5
elbrag <ellenbrage@outlook.com>
2017-10-10 10:09:04 +0200

    x

--- index.php
+++ index.php
@@ -18,40 +18,6 @@
 <div id="browse">
 
 
-  <div class="box">
-      <h3 class="profiletitle">Bowling &amp; pizza</h3>
-      <hr />
-      <img src="img/ellen.jpg" class="profilepic"/>
-      <div class="specifics">
-          <p><img src="img/place.png" />Jönköping centrum</p>
-          <br />
-          <p><img src="img/time.png" />24/7 kl 10.00</p>
-      </div>
-      <p class="description">Some text about events event evsnesabdija
-        cdasfaifhnjkn hfsfeoscndnvcdsoifjdsiccxjnzkcjnzjk
-        Hje här ör lite text om ingenting för att elenne ska ska hur detta ser ut stavar jag ens rtt???
-      </p>
-      <br />
-      <a href="event.php" class="seemore">more...</a>
-  </div>
-
-  <div class="box">
-      <h3 class="profiletitle">Bowling &amp; pizza</h3>
-      <hr />
-      <img src="img/ellen.jpg" class="profilepic"/>
-      <div class="specifics">
-          <p><img src="img/place.png" />Jönköping centrum</p>
-          <br />
-          <p><img src="img/time.png" />24/7 kl 10.00</p>
-      </div>
-      <p class="description">Some text about events event evsnesabdija
-        cdasfaifhnjkn hfsfeoscndnvcdsoifjdsiccxjnzkcjnzjk
-        Hje här ör lite text om ingenting för att elenne ska ska hur detta ser ut stavar jag ens rtt???
-      </p>
-      <br />
-      <a href="event.php" class="seemore">more...</a>
-  </div>
-
   <?php
       @ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
 
@@ -87,52 +53,6 @@
 
 
   ?>
-
-
-  <div class="box">
-      <h3 class="profiletitle">Gå på Jurassic Park-premiären</h3>
-      <hr />
-      <img src="img/ellen.jpg" class="profilepic"/>
-      <div class="specifics">
-          <p><img src="img/place.png" />SF Bergakungen</p>
-          <br />
-          <p><img src="img/time.png" />30/7 kl 20.30</p>
-      </div>
-      <p class="description">Some text about events event evsnesabdija
-        cdasfaifhnjkn hfsfeoscndnvcdsoifjdsiccxjnzkcjnzjk
-      </p>
-      <br />
-      <a href="event.php" class="seemore">more...</a>
-  </div>
-
-  <div class="box">
-      <h3 class="profiletitle">Bowling &amp; pizza</h3>
-      <hr />
-      <img src="img/ellen.jpg" class="profilepic"/>
-      <div class="specifics">
-          <p><img src="img/place.png" />Folkungagatan 156 tredje våningen portkod 4343</p>
-          <br />
-          <p><img src="img/time.png" />24/7 kl 10.00 eller kanske kl 13.00 vad tycker ni</p>
-      </div>
-      <p class="description">Some text about events event evsnesabdija
-        cdasfaifhnjkn hfsfeoscndnvcdsoifjdsiccxjnzkcjnzjk
-        Hje här ör lite text om ingenting för att elenne ska ska hur detta ser ut stavar jag ens rtt???
-      </p>
-      <br />
-      <a href="event.php" class="seemore">more...</a>
-  </div>
-
-  <div class="box">
-    <p class="description">Some text about events event evsnesabdija
-      cdasfaifhnjkn hfsfeoscndnvcdsoifjdsiccxjnzkcjnzjk
-    </p>
-  </div>
-
-  <div class="box">
-    <p class="description">Some text about events event evsnesabdija
-      cdasfaifhnjkn hfsfeoscndnvcdsoifjdsiccxjnzkcjnzjk
-    </p>
-  </div>
 
 </div>
 
