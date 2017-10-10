42d39463ef25447fff076dfe3ac76f03100b0e79
elbrag <ellenbrage@outlook.com>
2017-10-10 12:10:33 +0200

    created the structure of events in my profile

---
+++ myeventsheader.php
@@ -0,0 +1,10 @@
+<header id="whatevent">
+  <nav id="chooseeventtype">
+      <ul>
+        <li><a id="<?php echo ($current_page == 'hostedevents.php') ? 'active' : NULL ?>" href="hostedevents.php">Hosting</a></li>
+        <li><a id="<?php echo ($current_page == 'savedevents.php') ? 'active' : NULL ?>" href="savedevents.php">Saved</a></li>
+        <li><a id="<?php echo ($current_page == 'attendedevents.php') ? 'active' : NULL ?>" href="attendedevents.php">Attending</a></li>
+      </ul>
+  </nav>
+
+</header>
