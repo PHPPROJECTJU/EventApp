0944c44b74c71dd91f89944e74cbbe716492f866
Albinjoe <albinjoh1@gmail.com>
2017-10-12 11:47:31 +0200

    footer mupp

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 
             <footer>
+              <img src="img/skyline-1.png" class="footerimg">
                   <!--<p>© Julia Palm, Albin Johansson, Ellen Brage
                   <br/>Jönköping University <?php echo date("Y");?>
                 </p>-->
