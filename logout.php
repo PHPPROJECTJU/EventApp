1d2f3dd573e072dfebd9ead6fa56ec198734197f
Albinjoe <albinjoh1@gmail.com>
2017-10-12 17:37:16 +0200

    Logout fix

--- logout.php
+++ logout.php
@@ -7,12 +7,34 @@ if (!isset($_SESSION['username'])) {
 }
 ?>
 
-<?php
+<!DOCTYPE html>
+	  <html lang="en">
+	  <head>
+		    <meta charset="UTF-8">
+	      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	      <link rel="stylesheet" href="css/main.css">
+	      <link href="https://fonts.googleapis.com/css?family=Lato|Noto+Sans:300,400,600|Roboto:300,400,600,700" rel="stylesheet">
+	  </head>
 
-    session_destroy();
-    unset($_SESSION['username']);
-    //header("location:login.php");
+<body>
+		<main class="red" id="main">
 
-    printf("You are now logged out.<br>");
-    printf("<br><a href=login.php>Return to login page.</a>");
-?>
+              <?php
+                  session_destroy();
+                  unset($_SESSION['username']);
+                  printf("You are now logged out.<br>");
+                  printf("<br><a href=login.php>Return to login page.</a>");
+              ?>
+
+              <footer class="logoutfooter">
+                <img src="img/skyline-1.png" class="footerimg">
+                    <!--<p>© Julia Palm, Albin Johansson, Ellen Brage
+                    <br/>Jönköping University <?php echo date("Y");?>
+                  </p>-->
+              </footer>
+
+
+    </main>
+</body>
+<script type="text/javascript" src="js/menu.js"></script>
+</html>
