a11f203c5a2b01dae8a4b02d96892dd218ee754f
elbrag <ellenbrage@outlook.com>
2017-10-13 17:43:08 +0200

    fixed myprofile so it displays the right user

--- header.php
+++ header.php
@@ -7,6 +7,13 @@
 	      <link href="https://fonts.googleapis.com/css?family=Lato|Noto+Sans:300,400,600|Roboto:300,400,600,700" rel="stylesheet">
 	  </head>
 
+
+<?php
+
+$username = $_SESSION['username'];
+
+?>
+
 	  <body>
 		    <main id="main">
 	      <header id="mainheader">
@@ -14,7 +21,7 @@
 						<nav id="mainmenu">
 							<div id="close" onclick="closeNav()">×</div>
 							<ul>
-								<li><a class ="slide-effect" id="<?php echo ($current_page == 'profile.php') ? 'active' : NULL ?>" href="profile.php">My profile</a></li>
+								<li><a class ="slide-effect" id="<?php echo ($current_page == 'profile.php') ? 'active' : NULL ?>" href="profile.php"><?php echo $username; ?></a></li>
 								<li><a class ="slide-effect" id="<?php echo ($current_page == 'index.php' || $current_page == '') ? 'active' : NULL ?>" href="index.php">Browse</a></li>
 								<li><a class ="slide-effect" id="<?php echo ($current_page == 'about.php') ? 'active' : NULL ?>" href="about.php">About</a></li>
 								<li><a class ="slide-effect" id="<?php echo ($current_page == 'contact.php') ? 'active' : NULL ?>" href="contact.php">Contact</a></li>
--- profile.php
+++ profile.php
@@ -12,6 +12,10 @@
 ?>
 
 <?php
+
+$username = $_SESSION['username'];
+
+
     @ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
 
     if ($db->connect_error) {
@@ -20,29 +24,31 @@
         exit();
     }
 
-    $query = "SELECT User.UserName, User.Password, User.EmailAdress, User.FirstName, User.LastName, User.Birthdate, User.About, User.ProfilePicture
-              FROM User";
+    $query = "SELECT
+              User.EmailAdress, User.FirstName, User.LastName, User.Birthdate, User.About, User.ProfilePicture
+              FROM User
+              WHERE User.UserName = '$username'
+              ";
 
 
     $stmt = $db->prepare($query);
-    $stmt->bind_result($UserName, $Password, $EmailAdress, $FirstName, $LastName, $Birthdate, $About, $ProfilePicture);
+    $stmt->bind_result($EmailAdress, $FirstName, $LastName, $Birthdate, $About, $ProfilePicture);
     $stmt->execute();
 
     while ($stmt->fetch()) {
         echo "<div class='profilebox'>";
         echo "<img src='$ProfilePicture' id='personalprofilepic'/>";
-        echo "</br><h3 class='personalusername'>$UserName</h3></br></br>";
+        echo "</br><h3 class='personalusername'>$username</h3></br></br>";
         echo "Name:</br> $FirstName $LastName</br>";
         echo "</br>Birthdate:</br>$Birthdate</br>";
         echo "</br>Email:</br> $EmailAdress</br>";
         echo "</br>About:</br>$About</br>";
-        echo "</br></br><a class='profilebutton' href='editprofile.php'>Link to edit profile</a>";
+        echo "</br></br><a class='profilebutton' href='editprofile.php'>Edit profile</a>";
         echo "</div>";
     }
 
 ?>
-<a href="createprofile.php">Link to create profile</a>
-<a href="editprofile.php">Link to edit profile</a>
+
 
 <?php
   include("footer.php");
