be914f6135e0ca6157c6b607b6ee74b98142a585
elbrag <ellenbrage@outlook.com>
2017-10-16 19:32:18 +0200

    working on search function

--- index.php
+++ index.php
@@ -37,7 +37,33 @@
           exit();
       }
 
-      $query = "SELECT User.UserName, User.ProfilePicture, User.UserID, Event.EventID, Event.Title, Event.StartDate, Event.StartTime, Event.Information, Location.StreetAdress
+
+/*--Search function-----------------------------------------*/
+
+if (isset($_POST) && !empty($_POST)) {
+
+    $searchphrase = htmlentities($_POST['searchevent']);
+
+    $searchphrase = trim($searchphrase);
+
+    $searchphrase = mysqli_real_escape_string($db, $searchphrase);
+
+    $searchphrase = addslashes($searchphrase);
+
+
+    $search = "SELECT User.UserName, User.ProfilePicture, User.UserID, Event.EventID, Event.Title, Event.StartDate, Event.StartTime, Event.Information, Location.StreetAdress
+              FROM Event
+              WHERE Title LIKE" . $searchphrase . "
+              ";
+
+          $stmt = $db->prepare($search);
+          $stmt->bind_result($UserName, $ProfilePicture, $UserID, $EventID, $Title, $StartDate, $StartTime, $Information, $StreetAdress);
+          $stmt->execute();
+} else {
+
+/*--Getting stuff from database without searching-----------------------------------------*/
+
+      $getevent = "SELECT User.UserName, User.ProfilePicture, User.UserID, Event.EventID, Event.Title, Event.StartDate, Event.StartTime, Event.Information, Location.StreetAdress
                 FROM User
                 JOIN Event
                 ON User.UserID=Event.UserID
@@ -46,26 +72,29 @@
                 ORDER BY Event.EventID DESC
                 ";
 
-      $stmt = $db->prepare($query);
+      $stmt = $db->prepare($getevent);
       $stmt->bind_result($UserName, $ProfilePicture, $UserID, $EventID, $Title, $StartDate, $StartTime, $Information, $StreetAdress);
       $stmt->execute();
 
-      while ($stmt->fetch()) {
-          echo "<div class='box'>";
-          echo "<a name='". urldecode($UserID) ."'><h3 class='profiletitle'>$Title</h3></a>";
-          echo "<span class='pictureandname'>";
-          echo "<img src='$ProfilePicture' class='profilepic'/>";
-          echo "<a class='username' href='user.php?UserID= " . urlencode($UserID) . " '> $UserName </a>";
-          echo "</span>";
-          echo "<div class='specifics'>";
-          echo "<p><img src='img/place.png' />$StreetAdress</p> <br />";
-          echo "<p><img src='img/time.png' />$StartDate kl $StartTime</p>";
-          echo "</div>";
-          echo "<p class='description'>$Information</p>";
-          echo "<a class='seemore' href='event.php?EventID=" . urlencode($EventID) . " '>more...</a>";
-          echo "</div>";
-      }
 
+
+    } /*<--end of the if/else post isset statement*/
+
+    while ($stmt->fetch()) {
+        echo "<div class='box'>";
+        echo "<a name='". urldecode($UserID) ."'><h3 class='profiletitle'>$Title</h3></a>";
+        echo "<span class='pictureandname'>";
+        echo "<img src='$ProfilePicture' class='profilepic'/>";
+        echo "<a class='username' href='user.php?UserID= " . urlencode($UserID) . " '> $UserName </a>";
+        echo "</span>";
+        echo "<div class='specifics'>";
+        echo "<p><img src='img/place.png' />$StreetAdress</p> <br />";
+        echo "<p><img src='img/time.png' />$StartDate kl $StartTime</p>";
+        echo "</div>";
+        echo "<p class='description'>$Information</p>";
+        echo "<a class='seemore' href='event.php?EventID=" . urlencode($EventID) . " '>more...</a>";
+        echo "</div>";
+    }
   ?>
 
 </div>
