3c7b3247d31fc0c86fac3c62cd2f18cdabd2b5d7
elbrag <ellenbrage@outlook.com>
2017-10-23 22:49:15 +0200

    d

--- event.php
+++ event.php
@@ -127,7 +127,7 @@ function postComment($comment) {
     }
 }
 
-if (isset($_POST['postcomment']) && !empty($_POST['postcomment']) ) {
+if (isset($_POST) && !empty($_POST) ) {
   postComment($_POST['commentfield']);
 }
 
