d06d101317883458dd3b6f1366a4c6d9b93bf6b4
elbrag <ellenbrage@outlook.com>
2017-10-25 10:44:52 +0200

    event

--- event.php
+++ event.php
@@ -12,47 +12,54 @@
 
 <?php
 
-
-$EventID = trim($_GET['EventID']);
-
-
-    @ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
-
-    if ($db->connect_error) {
-        echo "could not connect: " . $db->connect_error;
-        printf("<br><a href=index.php>Return to home page </a>");
-        exit();
-    }
-
-    $query = "SELECT User.UserName, User.ProfilePicture, User.UserID, Event.Title, Event.StartDate, Event.StartTime, Event.Information, Location.StreetAdress
-              FROM User
-              JOIN Event
-              ON User.UserID=Event.UserID
-              JOIN Location
-              ON Event.LocationID=Location.LocationID
-              WHERE Event.EventID=$EventID
-              ";
-
-    $stmt = $db->prepare($query);
-    $stmt->bind_result($UserName, $ProfilePicture, $UserID, $Title, $StartDate, $StartTime, $Information, $StreetAdress);
-    $stmt->execute();
+function displayEvent(){
+  include("config.php");
 
 
-    while ($stmt->fetch()) {
-        echo "<a href='index.php#". urlencode($UserID) ."' class='goback'>&larr;</a>";
-        echo "<div class='eventpagebox'>";
-        echo "<h3 class='profiletitle'>$Title</h3>";
-        echo "<span class='pictureandname'>";
-        echo "<img src='$ProfilePicture' class='profilepic'/>";
-        echo "<a class='username' href='user.php?UserID= " . urlencode($UserID) . " '> $UserName </a>";
-        echo "</span>";
-        echo "<div class='specifics'>";
-        echo "<p><img src='img/place.png' />$StreetAdress</p> <br />";
-        echo "<p><img src='img/time.png' />$StartDate kl $StartTime</p>";
-        echo "</div>";
-        echo "<p class='description'>$Information</p>";
-        echo "</div>";
-    }
+  $EventID = trim($_GET['EventID']);
+
+
+      @ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
+
+      if ($db->connect_error) {
+          echo "could not connect: " . $db->connect_error;
+          printf("<br><a href=index.php>Return to home page </a>");
+          exit();
+      }
+
+      $query = "SELECT User.UserName, User.ProfilePicture, User.UserID, Event.Title, Event.StartDate, Event.StartTime, Event.Information, Location.StreetAdress
+                FROM User
+                JOIN Event
+                ON User.UserID=Event.UserID
+                JOIN Location
+                ON Event.LocationID=Location.LocationID
+                WHERE Event.EventID=$EventID
+                ";
+
+      $stmt = $db->prepare($query);
+      $stmt->bind_result($UserName, $ProfilePicture, $UserID, $Title, $StartDate, $StartTime, $Information, $StreetAdress);
+      $stmt->execute();
+
+
+      while ($stmt->fetch()) {
+          echo "<a href='index.php#". urlencode($UserID) ."' class='goback'>&larr;</a>";
+          echo "<div class='eventpagebox'>";
+          echo "<h3 class='profiletitle'>$Title</h3>";
+          echo "<span class='pictureandname'>";
+          echo "<img src='$ProfilePicture' class='profilepic'/>";
+          echo "<a class='username' href='user.php?UserID= " . urlencode($UserID) . " '> $UserName </a>";
+          echo "</span>";
+          echo "<div class='specifics'>";
+          echo "<p><img src='img/place.png' />$StreetAdress</p> <br />";
+          echo "<p><img src='img/time.png' />$StartDate kl $StartTime</p>";
+          echo "</div>";
+          echo "<p class='description'>$Information</p>";
+          echo "</div>";
+      }
+
+}
+
+displayEvent();
 
 ?>
 
@@ -75,7 +82,8 @@ $EventID = trim($_GET['EventID']);
 
 <?php
 
-if (isset($_POST) && !empty($_POST)) {
+function makeComment($comment){
+  include("config.php");
 
   @ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
 
@@ -87,44 +95,69 @@ if (isset($_POST) && !empty($_POST)) {
 
 
   $myusername = $_SESSION['username'];
+  $EventID = trim($_GET['EventID']);
 
-
-    $comment = htmlentities($_POST['commentfield']);
+    $comment = $comment;
     $comment = mysqli_real_escape_string($db, $comment);
 
     $commentquery = ("INSERT INTO Comments (Text, EventID, UserName)
-                  VALUES ('{$comment}', '{$EventID}', '{$myusername}')
+                  VALUES (?, ?, ?)
                   ");
 
-    $stmt = $db->prepare($commentquery);
-    $stmt->execute();
-
+    $stmt2 = $db->prepare($commentquery);
+    $stmt2->bind_param('sis', $comment, $EventID, $myusername);
+    $stmt2->execute();
 }
 
-$getcomment = ("SELECT Comments.Text, Comments.UserName, User.UserID, User.ProfilePicture
-          FROM Comments
-          JOIN User
-          ON Comments.UserName=User.UserName
-          WHERE EventID=$EventID
-          ");
-$stmt2 = $db->prepare($getcomment);
-$stmt2->bind_result($Text, $Commenter, $UserID, $Profilepic);
-$stmt2->execute();
 
-
-while ($stmt2->fetch()) {
-    echo "<div class='comment'>";
-    echo "<img src='$Profilepic' class='commenterpic'/>";
-    echo "<a href='user.php?UserID= " . urlencode($UserID) . " '> $Commenter </a> says";
-    echo "<div id='commentBox'>";
-    echo "<div class='commentarrow'></div>";
-    echo "<div id='commenttext'><p>";
-    echo $Text;
-    echo "</p></div>";
-    echo "</div>";
-    echo "</div>";
-    echo "</div>";
+if (isset($_POST['commentfield'])) {
+  makeComment($_POST['commentfield']);
 }
+
+
+
+function getComment(){
+  include("config.php");
+
+  @ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
+
+      if ($db->connect_error) {
+          echo "could not connect: " . $db->connect_error;
+          printf("<br><a href=index.php>Return to home page </a>");
+          exit();
+      }
+
+      $EventID = trim($_GET['EventID']);
+
+      $getcomment = ("SELECT Comments.Text, Comments.UserName, User.UserID, User.ProfilePicture
+                FROM Comments
+                JOIN User
+                ON Comments.UserName=User.UserName
+                WHERE EventID=$EventID
+                ");
+      $stmt3 = $db->prepare($getcomment);
+      $stmt3->bind_result($Text, $Commenter, $UserID, $Profilepic);
+      $stmt3->execute();
+
+      while ($stmt3->fetch()) {
+          echo "<div class='comment'>";
+          echo "<img src='$Profilepic' class='commenterpic'/>";
+          echo "<a href='user.php?UserID= " . urlencode($UserID) . " '> $Commenter </a> says";
+          echo "<div id='commentBox'>";
+          echo "<div class='commentarrow'></div>";
+          echo "<div id='commenttext'><p>";
+          echo $Text;
+          echo "</p></div>";
+          echo "</div>";
+          echo "</div>";
+          echo "</div>";
+      }
+}
+
+
+getComment();
+
+
 
 
 ?>
