d75e7b255f5b0177122f3b6f78593dae1bd6207d
elbrag <ellenbrage@outlook.com>
2017-10-25 16:13:13 +0200

    fee

---
+++ createevent.php
@@ -0,0 +1,72 @@
+<form action="" method="POST">
+  <div id="eventform">
+        <div class="row">
+             Name of Event<input type="text" name="eventname" class="eventregisterbar" placeholder="Name of Event" required>
+        </div>
+
+        <div class="row">
+            <div class="innerdatewrap"><p>Start date</p><input type="date" name="startdate" class="test" placeholder="Time for event" required></div>
+            <div class="innerdatewrap"><p>Start time</p><input type="time" name="starttime" class="test" placeholder="Time for event" required></div>
+
+        </div>
+        <div class="row">
+            <div class="innerdatewrap"><p>End date</p><input type="date" name="enddate" class="test" placeholder="Time for event" required></div>
+            <div class="innerdatewrap"><p>End time</p><input type="time" name="endtime" class="test" placeholder="Time for event" required></div>
+        </div>
+        <div class="row">
+          Describe your event<textarea rows="4" cols="80" type="textarea" name="description" class="eventregisterbartext" placeholder="Description of event" required></textarea>
+        </div>
+         <div>
+             <input type="submit" class="registerbutton" name="createevent" value="Create event">
+         </div>
+   </div>
+
+</form>
+</div>
+
+
+</div>
+
+</div>
+
+<?php
+
+
+function createEvent(){
+include("config.php");
+
+@ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
+
+if ($db->connect_error) {
+echo "could not connect: " . $db->connect_error;
+printf("<br><a href=index.php>Return to home page </a>");
+exit();
+}
+
+$eventname = trim($_POST['eventname']);
+$startdate = trim($_POST['startdate']);
+$enddate = trim($_POST['enddate']);
+$starttime = trim($_POST['starttime']);
+$endtim = trim($_POST['endtime']);
+$description = trim($_POST['description']);
+
+$eventname = addslashes($eventname);
+$startdate = addslashes($startdate);
+$enddate = addslashes($enddate);
+$starttime = addslashes($starttime);
+$endtim = addslashes($endtim);
+$description = addslashes($description);
+
+
+
+$stmt = $db->prepare("INSERT INTO Event (Event.Title, Event.StartDate, Event.EndDate, Event.StartTime, Event.EndTime, Event.Information)
+                   VALUES (?, ?, ?, ?, ?, ?)");
+$stmt->bind_param('ssssss', $eventname, $startdate, $enddate, $starttime, $endtim, $description);
+$stmt->execute();
+printf("Event created!");
+}
+
+if (isset($_POST['createevent'])) {
+createEvent();
+}
+?>
--- header.php
+++ header.php
@@ -47,122 +47,78 @@ $username = $_SESSION['username'];
 								       <h2>Create a new event!</h2>
 								</div>
 								 	<div class="modal-body">
-												 <form action="" method="POST">
-								           <div id="eventform">
-										             <div class="row">
-										               		Name of Event<input type="text" name="eventname" class="eventregisterbar" placeholder="Name of Event" required>
-										             </div>
-
-									               <div class="row">Select your region
-																	 	<select name="region" form="eventform" placeholder="Select region">
-																				<option value="" disabled selected>Select your region</option>
-																				<?php
-																				function getregions(){
-
-																						include ("config.php");
-																						@ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
-
-																						if ($db->connect_error) {
-																								echo "could not connect: " . $db->connect_error;
-																								printf("<br><a href=index.php>Return to home page </a>");
-																								exit();
-																						}
-
-																						$getregion = "SELECT State.state_id, State.state_name
-																											FROM State
-																											";
-
-																						$stmt = $db->prepare($getregion);
-																						$stmt->bind_result($regionid, $showregion);
-																						$stmt->execute();
-
-																						while ($stmt->fetch()) {
-																									echo "<option value='$regionid'>$showregion</option>";
-																						}
-																				};
-																           getregions(); 
-																         ?>
-																 		</select>
-																</div>
-
-																 <div class="row">
-											               <div class="innerdatewrap"><p>Start date</p><input type="date" name="startdate" class="test" placeholder="Time for event" required></div>
-																		 <div class="innerdatewrap"><p>Start time</p><input type="time" name="starttime" class="test" placeholder="Time for event" required></div>
-
-										             </div>
-										             <div class="row">
-											               <div class="innerdatewrap"><p>End date</p><input type="date" name="enddate" class="test" placeholder="Time for event" required></div>
-																		 <div class="innerdatewrap"><p>End time</p><input type="time" name="endtime" class="test" placeholder="Time for event" required></div>
-										             </div>
-										             <div class="row">
-										               Describe your event<textarea rows="4" cols="80" type="textarea" name="description" class="eventregisterbartext" placeholder="Description of event" required></textarea>
-																 </div>
-																 <div class="row">
-										               Select tags
-																		 	<select name="tag" form="eventform" placeholder="Select tags">
-																					<option value="" disabled selected>Select tags</option>
-																	        <option value="firsttag">First option</option>
-																	 		</select>
-																	</div>
-															 		<div>
-																	 		<input type="submit" class="registerbutton" name="createevent" value="Create event">
-																 	</div>
+										<form action="" method="POST">
+											<div id="eventform">
+														<div class="row">
+																 Name of Event<input type="text" name="eventname" class="eventregisterbar" placeholder="Name of Event" required>
 														</div>
 
-								         </form>
-									 </div>
+														<div class="row">
+																<div class="innerdatewrap"><p>Start date</p><input type="date" name="startdate" class="test" placeholder="Time for event" required></div>
+																<div class="innerdatewrap"><p>Start time</p><input type="time" name="starttime" class="test" placeholder="Time for event" required></div>
+
+														</div>
+														<div class="row">
+																<div class="innerdatewrap"><p>End date</p><input type="date" name="enddate" class="test" placeholder="Time for event" required></div>
+																<div class="innerdatewrap"><p>End time</p><input type="time" name="endtime" class="test" placeholder="Time for event" required></div>
+														</div>
+														<div class="row">
+															Describe your event<textarea rows="4" cols="80" type="textarea" name="description" class="eventregisterbartext" placeholder="Description of event" required></textarea>
+														</div>
+														 <div>
+																 <input type="submit" class="registerbutton" name="createevent" value="Create event">
+														 </div>
+											 </div>
+
+										</form>
+										</div>
 
 
-					   </div>
+										</div>
 
-					 </div>
+										</div>
 
-					 <?php
+										<?php
 
 
-					 function createEvent(){
-						 include("config.php");
+										function createEvent(){
+										include("config.php");
 
-						 @ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
+										@ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
 
-				         if ($db->connect_error) {
-				             echo "could not connect: " . $db->connect_error;
-				             printf("<br><a href=index.php>Return to home page </a>");
-				             exit();
-				         }
+										if ($db->connect_error) {
+										echo "could not connect: " . $db->connect_error;
+										printf("<br><a href=index.php>Return to home page </a>");
+										exit();
+										}
 
-						        $eventname = trim($_POST['eventname']);
-						        $region = trim($_POST['region']);
-						        $startdate = trim($_POST['startdate']);
+										$eventname = trim($_POST['eventname']);
+										$startdate = trim($_POST['startdate']);
 										$enddate = trim($_POST['enddate']);
 										$starttime = trim($_POST['starttime']);
 										$endtim = trim($_POST['endtime']);
 										$description = trim($_POST['description']);
-						        $tag = trim($_POST['tag']);
 
 										$eventname = addslashes($eventname);
-										$region = addslashes($region);
 										$startdate = addslashes($startdate);
 										$enddate = addslashes($enddate);
 										$starttime = addslashes($starttime);
 										$endtim = addslashes($endtim);
 										$description = addslashes($description);
-										$tag = addslashes($tag);
 
 
 
-						         $stmt = $db->prepare("INSERT INTO Event (Event.Title, Event.StartDate, Event.EndDate, Event.StartTime, Event.EndTime, Event.Information, Event.Tags)
-										 												VALUES (?, ?, ?, ?, ?, ?, ?)");
-						         $stmt->bind_param('siiiiss', $eventname, $region, $startdate, $enddate, $starttime, $endtim, $description, $tag);
-						         $stmt->execute();
-						         printf("Event created!");
-					 }
+										$stmt = $db->prepare("INSERT INTO Event (Event.Title, Event.StartDate, Event.EndDate, Event.StartTime, Event.EndTime, Event.Information)
+																			 VALUES (?, ?, ?, ?, ?, ?)");
+										$stmt->bind_param('ssssss', $eventname, $startdate, $enddate, $starttime, $endtim, $description);
+										$stmt->execute();
+										printf("Event created!");
+										}
 
-if (isset($_POST['createevent'])) {
-	createEvent();
-}
-
-			     ?>
+										if (isset($_POST['createevent'])) {
+										createEvent();
+										}
+										?>
 
 	      </header>
 
