98f0efe5b39e48cc2052cac08279e17a2507053c
Albinjoe <albinjoh1@gmail.com>
2017-10-27 13:03:37 +0200

    fix

--- editprofile.php
+++ editprofile.php
@@ -9,64 +9,131 @@
 ?>
 
 <!--Formfields should only be required if there is no input from before-->
-<div id="createprofile">
-    <form method="POST" name="createprofile" action="createprofile.php">
-        <h1>User:<!--use php to get usr name--><br>Edit your profile</h1>
-          <br>
-          <input type="name" name="firstname" placeholder="Firstname"/>
-          <br>
-          <input type="name" name="lastname" placeholder="Lastname"/>
-          <br>
-          <input type="date" name="bday" min="<?php $date ?>"/>
-          <br>
+<?php echo $username;?>
+<form action="editprofile.php" method="POST" enctype="multipart/form-data">
+  <table id="registerform">
+    <tr>
+      <td>
+        <input type="name" name="username" class="registerbar" placeholder="Username"/>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <input type="name" name="password" class="registerbar" placeholder="Password"/>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <input type="name" name="firstname" class="registerbar" placeholder="Firstname" />
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <input type="name" name="lastname" class="registerbar" placeholder="Lastname" />
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <input type="date" name="bday" class="registerbar" placeholder="Birthdate" />
+      </td>
+    </tr>
+    <tr>
+          <td>
+              <p>Choose a profile picture:</p>
+                  <input type="file" name="fileupload" id="fileupload">
+                  <br />
+          </td>
+    </tr>
+    <tr>
+        <td>
+            <textarea name="about" placeholder="Tell me something about yourself!" class="registerbar"></textarea>
+        </td>
+    </tr>
+    <tr>
+      <td><input type="submit" class="registerbutton" name="submit" value="Register"></td>
+    </tr>
+  </table>
+</form>
 
-          <p>Upload your own profile picture:</p>
-          <!--Don't really know how to fix this one, heh...-->
-          <!--Uploaded pic should get the id of the usr that uploaded the pic and be displayed-->
-          <input type="file" name="fileupload" id="fileupload">
 
-          <h3>About me</h3>
-          <textarea name="about" placeholder="Tell me something about yourself!"></textarea>
-          <br>
-          <input type="submit" name="submit" value="Update" />
-    </form>
-</div>
-
-<?php
-include ("footer.php");
- ?>
 
 <!--Same code as in create profile but changed INSERT into UPDATE and took away required message-->
  <?php
-/*
-   #Checks if data has been submitted
-   if (isset($_POST['username'], $_POST['password'], $_POST['firstname'], $_POST['lastname'], $_POST['email'], $_POST['bday'], $_POST['fileupload'], $_POST['about'])) {
-   #Get data from form
-       $Username = trim($_POST['username']);
-       $Password = sha1($_POST['password']);
-       $FirstName = trim($_POST['firstname']);
-       $LastName = trim($_POST['lastname']);
-       $Email = trim($_POST['email']);
-       $Bday = trim($_POST['bday']);
-       $ProfilePicture = trim($_POST['fileupload']);
-       $About = trim($_POST['about']);
 
- //--------------DATABASE CONNECTION--------------//
-       @ $db = new mysqli('localhost', 'root', '', 'EventApp');
+function updatetheuser(){
+ include("config.php");
 
-       if ($db->connect_error) {
-           echo "could not connect: " . $db->connect_error;
-           printf("<br><a href='index.php'>Return to home page </a>");
-           exit();
+ @ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
+
+ if ($db->connect_error) {
+     echo "could not connect: " . $db->connect_error;
+     printf("<br><a href=index.php>Return to home page </a>");
+     exit();
+ }
+
+ $sessionuser = $_SESSION['username'];
+
+ #Getting the image upload:
+ if (isset($_FILES['fileupload'])) {
+
+       $allowedextensions = array('jpg', 'jpeg', 'gif', 'png');
+
+       $extension = strtolower(substr($_FILES['fileupload']['name'], strrpos($_FILES['fileupload']['name'], '.') + 1));
+
+       $error = array ();
+
+       if(in_array($extension, $allowedextensions) === false){
+         $error[] = 'Not an allowed file format.';
        }
- //-----------------------------------------------//
 
-       $stmt = $db->prepare("UPDATE User ('UserID', 'Username', 'Password', 'EmailAdress', 'FirstName', 'LastName', 'Birthdate', 'About', 'ProfilePicture')
-                             VALUES (null, ?, ?, ?, ?, ?, ?, ?, ?)");
-       $stmt->bind_param('isssssiss', $Username, $Password, $FirstName, $LastName, $Email, $Bday, $ProfilePicture, $About);
-       $stmt->execute();
-       printf("<br>User added!");
-       printf("<br><a href=index.php>Return to home page </a>");
-     }
-*/
+       if($_FILES['fileupload']['size'] > 1000000){
+         $error[]='This file is over the 1MB limit.';
+       }
+
+       if(empty($error)){
+         move_uploaded_file($_FILES['fileupload']['tmp_name'], "profilepics/user$UserID.$extension");
+       } else {
+           foreach ($error as $err){
+             echo $err;
+           }
+       }
+ } else {
+   echo "not uploaded";
+ }
+
+ $Username = trim($_POST['username']);
+ $Password = trim($_POST['password']);
+ $firstname = trim($_POST['firstname']);
+ $lastname = trim($_POST['lastname']);
+ $bday = trim($_POST['bday']);
+ $fileupload = "user".$UserID.".".$extension;
+ $about = trim($_POST['about']);
+
+ $Username = addslashes($Username);
+ $Password = addslashes($Password);
+ $firstname = addslashes($firstname);
+ $lastname = addslashes($lastname);
+ $bday = addslashes($bday);
+ $fileupload = addslashes($fileupload);
+ $about = addslashes($about);
+
+ $stmt = $db->prepare("UPDATE User
+                       SET UserName='$Username', Password='$Password', FirstName='$firstname', LastName='$lastname', Birthdate='$bday', ProfilePicture='profilepics/$fileupload', About='$about'
+                       WHERE User.UserName=$sessionuser");
+ $stmt->bind_param('sssssss', $Username, $Password, $firstname, $lastname, $bday, $fileupload, $about);
+ $stmt->execute();
+
+ ?>
+ <script>
+     window.location.href = "index.php";
+ </script>
+ <?php
+}
+
+    if (isset($_POST['submit'])) {
+
+    updatetheuser();
+    }
+
+    include ("footer.php");
  ?>
