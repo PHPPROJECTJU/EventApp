4b6debd7eb28919ec9d624d02373617d9276d64f
elbrag <ellenbrage@outlook.com>
2017-10-29 22:31:03 +0100

    footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 
             <footer>
-              <img src="img/skyline-1.png" class="footerimg">
+              <img src="img/skyline-3.png" class="footerimg">
                   <!--<p>© Julia Palm, Albin Johansson, Ellen Brage
                   <br/>Jönköping University <?php echo date("Y");?>
                 </p>-->
