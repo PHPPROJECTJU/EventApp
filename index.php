13caf4c479c5fd5ee7a34d1b549c82fe14f5ac84
Albinjoe <albinjoh1@gmail.com>
2017-11-02 11:50:35 +0100

    fix

--- index.php
+++ index.php
@@ -22,6 +22,7 @@
     </div>
     <ul id='regionmenu'>
       <div class="liwrapper">
+        <form name="chooseregion">
         <?php
             @ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
 
@@ -44,6 +45,7 @@
                   echo "<a class='regionbuttons' href='index.php?regionid=" . urlencode($regionid) . "'><li>$showregion</li></a>";
               }
          ?>
+       </form>
        </div>
      </ul>
 </div>
@@ -89,10 +91,9 @@ if (isset($_POST['search']) && !empty($_POST['searchevent'])) {
           $stmt = $db->prepare($search);
           $stmt->bind_result($UserName, $ProfilePicture, $UserID, $EventID, $Title, $StartDate, $StartTime, $Information, $StreetAdress, $cityname);
           $stmt->execute();
-} else if (isset($_POST['regions']){
+} else {
 
-/*----------Getting stuff from database depending on chosen location-----------------------------------------*/
-
+/*--Getting stuff from database without searching-----------------------------------------*/
 
       $getevent = "SELECT User.UserName, User.ProfilePicture, User.UserID, Event.EventID, Event.Title, DATE_FORMAT(StartDate, '%D %M, %Y') AS `StartDate`, DATE_FORMAT(`StartTime`, '%H:%i') AS `StartTime`, Event.Information, Event.StreetAdress, City.city_name
                 FROM User
@@ -125,42 +126,7 @@ if (isset($_POST['search']) && !empty($_POST['searchevent'])) {
         echo "<p class='description'>$Information</p>";
         echo "<a class='seemore' href='event.php?EventID=" . urlencode($EventID) . " '>more...</a>";
         echo "</div>";
-    } else {
-
-    /*--Getting stuff from database without searching-----------------------------------------*/
-
-          $getevent = "SELECT User.UserName, User.ProfilePicture, User.UserID, Event.EventID, Event.Title, DATE_FORMAT(StartDate, '%D %M, %Y') AS `StartDate`, DATE_FORMAT(`StartTime`, '%H:%i') AS `StartTime`, Event.Information, Event.StreetAdress, City.city_name
-                    FROM User
-                    JOIN Event
-                    ON User.UserID=Event.UserID
-                    JOIN City
-                    ON Event.city_id=City.city_id
-                    ORDER BY Event.EventID DESC
-                    ";
-
-          $stmt = $db->prepare($getevent);
-          $stmt->bind_result($UserName, $ProfilePicture, $UserID, $EventID, $Title, $StartDate, $StartTime, $Information, $StreetAdress, $cityname);
-          $stmt->execute();
-
-
-
-        } /*<--end of the if/else post isset statement*/
-
-        while ($stmt->fetch()) {
-            echo "<div class='box'>";
-            echo "<a name='". urldecode($UserID) ."'><h3 class='profiletitle'>$Title</h3></a>";
-            echo "<span class='pictureandname'>";
-            echo "<img src='$ProfilePicture' class='profilepic'/>";
-            echo "<a class='username' href='user.php?UserID= " . urlencode($UserID) . " '> $UserName </a>";
-            echo "</span>";
-            echo "<div class='specifics'>";
-            echo "<p><img src='img/place-black.png' />$StreetAdress, $cityname</p> <br />";
-            echo "<p><img src='img/time-black.png' />$StartDate kl $StartTime</p>";
-            echo "</div>";
-            echo "<p class='description'>$Information</p>";
-            echo "<a class='seemore' href='event.php?EventID=" . urlencode($EventID) . " '>more...</a>";
-            echo "</div>";
-        }
+    }
   ?>
 
 </div>
