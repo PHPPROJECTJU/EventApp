d55c38af03a33b7976bb7b0e103beae0282776fd
Albinjoe <albinjoh1@gmail.com>
2017-11-06 14:40:47 +0100

    Försök till update user fix. Har inte lyckats i databasen än for some reason...

--- editprofile.php
+++ editprofile.php
@@ -9,50 +9,67 @@
 ?>
 
 <!--Formfields should only be required if there is no input from before-->
+<?php $username = $_SESSION['username'];?>
+
+<?php
+    $username = $_SESSION['username'];
+
+    include("config.php");
+
+    @ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
+
+    if ($db->connect_error) {
+    echo "could not connect: " . $db->connect_error;
+    printf("<br><a href=index.php>Return to home page </a>");
+    exit();
+    }
+
+    $query = ("SELECT User.UserName, User.FirstName, User.LastName, User.Birthdate, User.About
+              FROM User
+              WHERE User.UserName = '{$username}'
+              ");
+
+    $stmt11 = $db->prepare($query);
+    $stmt11->bind_result($showusername, $showfirstname, $showlastname, $showbirthdate, $showabout);
+    $stmt11->execute();
+    $stmt11->store_result();
+    $stmt11->fetch();
+?>
+
 <form action="editprofile.php" method="POST" enctype="multipart/form-data">
   <table id="registerform">
     <tr>
-      <td>
-        <input type="name" name="username" class="registerbar" placeholder="Username"/>
+      <td><h4 class="eventheaders">Firstname</h4>
+        <input type="name" name="firstname" class="eventregisterbar" placeholder="<?php echo $showfirstname; ?>" />
       </td>
     </tr>
     <tr>
-      <td>
-        <input type="name" name="password" class="registerbar" placeholder="Password"/>
+      <td><h4 class="eventheaders">Lastname</h4>
+        <input type="name" name="lastname" class="eventregisterbar" placeholder="<?php echo $showlastname; ?>" />
       </td>
     </tr>
     <tr>
-      <td>
-        <input type="name" name="firstname" class="registerbar" placeholder="Firstname" />
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <input type="name" name="lastname" class="registerbar" placeholder="Lastname" />
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <input type="date" name="bday" class="registerbar" placeholder="Birthdate" />
+      <td><h4 class="eventheaders">Birthdate</h4>
+        <input type="date" name="bday" class="eventregisterbar"/>
       </td>
     </tr>
     <tr>
           <td>
               <p>Choose a profile picture:</p>
               <div class="fileUpload">
-                <div class="uploadbutton">Upload</div>
+                <div class="uploadbuttondark">Upload</div>
                 <input type="file" name="fileupload">
               </div>
-
+                  <br/>
           </td>
     </tr>
     <tr>
         <td>
-            <textarea name="about" placeholder="Tell me something about yourself!" class="registerbar"></textarea>
+            <textarea type="text" name="about" class="eventregisterbar80" style="height: 100px;"><?php echo $showabout; ?></textarea>
         </td>
     </tr>
     <tr>
-      <td><input type="submit" class="blockbutton" name="submit" value="Register"></td>
+      <td><input type="submit" class="colorbutton" name="submit" value="Update profile"></td>
     </tr>
   </table>
 </form>
@@ -61,78 +78,6 @@
 
 <!--Same code as in create profile but changed INSERT into UPDATE and took away required message-->
  <?php
-
-function updatetheuser(){
- include("config.php");
-
- @ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
-
- if ($db->connect_error) {
-     echo "could not connect: " . $db->connect_error;
-     printf("<br><a href=index.php>Return to home page </a>");
-     exit();
- }
-
- $sessionuser = $_SESSION['username'];
-
- #Getting the image upload:
- if (isset($_FILES['fileupload'])) {
-
-       $allowedextensions = array('jpg', 'jpeg', 'gif', 'png');
-
-       $extension = strtolower(substr($_FILES['fileupload']['name'], strrpos($_FILES['fileupload']['name'], '.') + 1));
-
-       $error = array ();
-
-       if(in_array($extension, $allowedextensions) === false){
-         $error[] = 'Not an allowed file format.';
-       }
-
-       if($_FILES['fileupload']['size'] > 1000000){
-         $error[]='This file is over the 1MB limit.';
-       }
-
-       if(empty($error)){
-         move_uploaded_file($_FILES['fileupload']['tmp_name'], "profilepics/user$UserID.$extension");
-       } else {
-           foreach ($error as $err){
-             echo $err;
-           }
-       }
- } else {
-   echo "not uploaded";
- }
-
- $Username = trim($_POST['username']);
- $Password = trim($_POST['password']);
- $firstname = trim($_POST['firstname']);
- $lastname = trim($_POST['lastname']);
- $bday = trim($_POST['bday']);
- $fileupload = "user".$UserID.".".$extension;
- $about = trim($_POST['about']);
-
- $Username = addslashes($Username);
- $Password = addslashes($Password);
- $firstname = addslashes($firstname);
- $lastname = addslashes($lastname);
- $bday = addslashes($bday);
- $fileupload = addslashes($fileupload);
- $about = addslashes($about);
-
- $stmt = $db->prepare("UPDATE User
-                       SET UserName='$Username', Password='$Password', FirstName='$firstname', LastName='$lastname', Birthdate='$bday', ProfilePicture='profilepics/$fileupload', About='$about'
-                       WHERE User.UserName=$username");
- $stmt->bind_param('sssssss', $Username, $Password, $firstname, $lastname, $bday, $fileupload, $about);
- $stmt->execute();
-
- ?>
-
- <script type="text/javascript" src="js/index.js"></script>
- <script>
-     window.location.href = "index.php";
- </script>
- <?php
-}
 
     if (isset($_POST['submit'])) {
 
--- functions.php
+++ functions.php
@@ -826,6 +826,80 @@ function finishtheuser(){
         <?php
   }
 
+  /*-----------------update the user------------------*/
+
+
+  function updatetheuser(){
+   include("config.php");
+
+   @ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
+
+   if ($db->connect_error) {
+       echo "could not connect: " . $db->connect_error;
+       printf("<br><a href=index.php>Return to home page </a>");
+       exit();
+   }
+
+   $sessionuser = $_SESSION['username'];
+
+   #Getting the image upload:
+   if (isset($_FILES['fileupload'])) {
+
+         $allowedextensions = array('jpg', 'jpeg', 'gif', 'png');
+
+         $extension = strtolower(substr($_FILES['fileupload']['name'], strrpos($_FILES['fileupload']['name'], '.') + 1));
+
+         $error = array ();
+
+         if(in_array($extension, $allowedextensions) === false){
+           $error[] = 'Not an allowed file format.';
+         }
+
+         if($_FILES['fileupload']['size'] > 1000000){
+           $error[]='This file is over the 1MB limit.';
+         }
+
+         if(empty($error)){
+           move_uploaded_file($_FILES['fileupload']['tmp_name'], "profilepics/user$UserID.$extension");
+         } else {
+             foreach ($error as $err){
+               echo $err;
+             }
+         }
+   } else {
+     echo "not uploaded";
+   }
+
+
+   $firstname = trim($_POST['firstname']);
+   $lastname = trim($_POST['lastname']);
+   $bday = trim($_POST['bday']);
+   $fileupload = "user".$UserID.".".$extension;
+   $about = trim($_POST['about']);
+
+
+   $firstname = addslashes($firstname);
+   $lastname = addslashes($lastname);
+   $bday = addslashes($bday);
+   $fileupload = addslashes($fileupload);
+   $about = addslashes($about);
+
+   $stmt = $db->prepare("UPDATE User
+                         SET FirstName='$firstname', LastName='$lastname', Birthdate='$bday', ProfilePicture='profilepics/$fileupload', About='$about'
+                         WHERE UserName=$sessionuser");
+   $stmt->bind_param('ssss', $firstname, $lastname, $bday, $fileupload, $about);
+   $stmt->execute();
+
+   ?>
+   <script>
+       window.location.href = "index.php";
+   </script>
+   <?php
+  }
+
+  /*-----------------edit profile-------------------*/
+
+
 /*Event comments*/
 
 #post a new comment
